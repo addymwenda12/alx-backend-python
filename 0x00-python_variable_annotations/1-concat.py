38e88dc167b9416d7c10783a44bdc7f5e91dc867
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-03-15 11:02:56 +0000

    Added function that returns a concatenated string

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Function that that takes a string str1
+and a string str2 as arguments and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Adds two strings and returns the result as a concatenated string.
+
+    :param str1: The first string.
+    :param str2: The second string.
+    :return: A concatenated string.
+    """
+    return (str1 + str2)
