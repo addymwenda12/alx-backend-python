ac2164e25ab84c0f8c2ae9faf0d0d52104473d0d
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-03-15 10:43:23 +0000

    Added function that add flooat numbers and returns the result

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Function that returns the sum of two arguments"""
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two float numbers and returns the result as a float.
+
+    :param a: The first float number.
+    :param b: The second float number.
+    :return: The sum of the two float numbers.
+    """
+    return (a + b)
