53c5003b3e3188d15bca268530e1411a65137202
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-03-15 11:24:50 +0000

    Returns the string representation of a float number

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Function that takes a float n as argument and
+returns the string representation of the float
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of the given float number.
+
+    :param n: The float number.
+    :return: The string representation of the given float number
+    """
+    return (to_str(n))
