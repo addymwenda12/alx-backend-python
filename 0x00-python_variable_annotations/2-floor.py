acb3e53c9607e604b7a43ebd32339cdbba33e362
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-03-15 11:12:34 +0000

    Added an argument that returns type-annotated function of the float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Function that takes a float n as argument and returns the floor of the float
+"""
+
+
+def floor(n: float) -> float:
+    """
+    Takes a float as an argument and returns the floor of the float
+
+    :param: n as the a float argument
+    """
+    return (n)
