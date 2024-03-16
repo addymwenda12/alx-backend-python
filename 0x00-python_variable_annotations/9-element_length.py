7b77d711500f19ffd9de18e53d048e158038446d
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-03-16 11:15:45 +0000

    Function that takes a list and returns a list of tuples

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -2,16 +2,16 @@
 """
 Function parameters that return values with the appropriate types
 """
-from typing import List, Tuple
+from typing import List, Sequence, Tuple
 
 
-def element_length(lst: List[object]) -> List[Tuple[object, int]]:
+def element_length(lst: Sequence[object]) -> List[Tuple[object, int]]:
     """
     This function takes a list of objects as an argument
     and returns a list of tuples.
     Each tuple contains an object from the input list and its length.
 
-    :param lst: A list of objects
+    :param lst: A sequence of objects
     :return: A list of tuples containing objects and their lengths
     """
     return [(i, len(i)) for i in lst]
