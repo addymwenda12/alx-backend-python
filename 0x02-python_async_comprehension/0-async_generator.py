e2b1233e1dffc8f8b003cb90f09be2127c305892
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-03-20 15:00:04 +0300

    Fixed to follow pycodestyle validation

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
-import asyncio
-import random
 """
 This module contains an asynchronous generator
 function that generates random numbers.
 """
 
+import asyncio
+import random
+from typing import AsyncGenerator
 
-async def async_generator():
+
+async def async_generator() -> AsyncGenerator[float, None]:
     """
-    Asynchronous generator that
-    yields a random number between 0 and 10
+    Asynchronous generator that yields a random number
+    between 0 and 10
     """
     for _ in range(10):
         await asyncio.sleep(1)
