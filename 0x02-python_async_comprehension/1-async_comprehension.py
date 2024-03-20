4ae1b7f13ae93be25028a566b8c1219ea18f752f
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-03-20 15:12:53 +0300

    Followed pycodestyle rule

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
-import asyncio
-from typing import List
-
-async_generator = __import__('0-async_generator').async_generator
 """
 This module contains an asynchronous generator function
 `async_generator`. 
 The purpose of this function is to generate random numbers.
 """
 
+import asyncio
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
 
 async def async_comprehension() -> List[float]:
     """
     Collects 10 random numbers using an async comprehension
-    over async_generator,
-    then returns the 10 random numbers.
+    over async_generator, then returns the 10 random numbers.
     """
     return [i async for i in async_generator()][:10]
