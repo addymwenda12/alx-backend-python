c36f820c4cb57559d2231ed6e728ce37348c29e9
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-03-20 14:12:05 +0300

    Asynchronous generator that generate random numbers

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+import asyncio
+import random
+"""
+This module contains an asynchronous generator
+function that generates random numbers.
+"""
+
+
+async def async_generator():
+    """
+    Asynchronous generator that
+    yields a random number between 0 and 10
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
