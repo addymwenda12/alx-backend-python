0ad1792ab9f5bddfe17169047da6b30d29160113
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-03-20 14:31:38 +0300

    Asynch generator thatreturn 10 random numbers

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,18 @@
+import asyncio
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+"""
+This module contains an asynchronous generator function
+`async_generator`. 
+The purpose of this function is to generate random numbers.
+"""
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Collects 10 random numbers using an async comprehension
+    over async_generator,
+    then returns the 10 random numbers.
+    """
+    return [i async for i in async_generator()][:10]
